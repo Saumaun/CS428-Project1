fd04f28ddcfc261e66c321a25bb857cee59f4515
Saumaun <svahed3@uic.edu>
2019-09-14 14:48:09 -0500

    Project 1 Files

---
+++ Project 1 Book/Assets/TargetAudio.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class TargetAudio : MonoBehaviour, ITrackableEventHandler
+{
+    public TrackableBehaviour t;
+    public AudioSource a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = GetComponent<TrackableBehaviour>();
+        if (t)
+            t.RegisterTrackableEventHandler(this);
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status prev, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED || newStatus == TrackableBehaviour.Status.TRACKED || newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            a.Play();
+        }
+        else
+        {
+            a.Stop();
+        }
+    }
+}
+ 
---
+++ Project 1 Book/Assets/button.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class button : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject buttonObject;
+    public GameObject Model;
+    public AudioSource a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonObject = GameObject.Find("VirtualButton");
+        Model = GameObject.Find("Holden");
+        Model.SetActive(false);
+        buttonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Model.SetActive(true);
+        a.Play();
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Model.SetActive(false);
+        a.Stop();
+    }
+}
